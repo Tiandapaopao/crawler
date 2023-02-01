70e473c98850086185b2ba9f1ca7b46916421cde
yusheng <991269526@qq.com>
2023-02-01 09:02:39 +0000

    text handle

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Tiandapaopao/crawler
 
 go 1.19
+
+require (
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,27 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	res, err := http.Get(url)
-	if err != nil {
-		fmt.Println("fetch url error:%v", err)
-		return
-	}
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		fmt.Println("error status code:%v", res.StatusCode)
-		return
-	}
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := Fetch(url)
+
 	if err != nil {
 		fmt.Println("read content failed:%v", err)
 		return
 	}
-	fmt.Println("body:", string(body))
+
+	fmt.Println(string(body))
+}
+
+// tag v0.0.4
+func Fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
